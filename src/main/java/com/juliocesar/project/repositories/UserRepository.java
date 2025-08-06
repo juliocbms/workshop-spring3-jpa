a62d4b96165cf1cb9e081216fd2bc36df25de15d
juliocbms <juliocesar13b@gmail.com>
2025-08-05 21:45:14 -0300

    Service layer, component registration

--- src/main/java/com/juliocesar/project/repositories/UserRepository.java
+++ src/main/java/com/juliocesar/project/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.juliocesar.project.repositories;
 
 import com.juliocesar.project.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
 
