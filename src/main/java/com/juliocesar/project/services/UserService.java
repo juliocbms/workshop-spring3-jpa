b36f71e9fddcba795bbb200a8f37c6e6b5f4df99
juliocbms <juliocesar13b@gmail.com>
2025-08-08 20:58:57 -0300

    Exception handling - findById

--- src/main/java/com/juliocesar/project/services/UserService.java
+++ src/main/java/com/juliocesar/project/services/UserService.java
@@ -2,9 +2,11 @@ package com.juliocesar.project.services;
 
 import com.juliocesar.project.entities.User;
 import com.juliocesar.project.repositories.UserRepository;
+import com.juliocesar.project.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.ReadOnlyFileSystemException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
