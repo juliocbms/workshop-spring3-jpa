61f4064b803b7a2549b1514cbc3d88a411db1e22
juliocbms <juliocesar13b@gmail.com>
2025-08-08 21:13:40 -0300

    Exception handling - update

--- src/main/java/com/juliocesar/project/services/UserService.java
+++ src/main/java/com/juliocesar/project/services/UserService.java
@@ -4,6 +4,7 @@ import com.juliocesar.project.entities.User;
 import com.juliocesar.project.repositories.UserRepository;
 import com.juliocesar.project.services.exceptions.DatabaseException;
 import com.juliocesar.project.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -46,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw  new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
